075f461e59654c7f5020e0860c52631f261a981c
AlexDuqu3 <alextu12@gmail.com>
2021-04-08 09:11:36 +0100

    ola

--- src/com/eisoft/moneyapp/models/Conta.java
+++ src/com/eisoft/moneyapp/models/Conta.java
@@ -1,6 +1,43 @@
 package com.eisoft.moneyapp.models;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Conta {
     public String nomeUtilizador;
     public float saldo;
+    private ArrayList<Movimentos> movimentos;
+
+
+    public Conta(String nomeUtilizador, float saldo) {
+        this.nomeUtilizador = nomeUtilizador;
+        this.saldo = saldo;
+        this.movimentos = new ArrayList<Movimentos>();
+    }
+
+    public String getNomeUtilizador() {
+        return nomeUtilizador;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public ArrayList<Movimentos> getMovimentos() {
+        return movimentos;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void adicionarSaldo(float saldo){
+        if (saldo <= 0) return;
+        setSaldo(saldo + this.saldo);
+    }
+
+    public void subtrairSaldo(float saldo){
+        if (saldo <= 0) return;
+        setSaldo(saldo - this.saldo);
+    }
 }
